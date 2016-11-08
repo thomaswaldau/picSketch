f40eb4ee74df04ad6024f95da2fbc49196ff94b4
thomaswaldau <thwa@hotmail.se>
2016-11-08 13:27:40 +0100

    code
    Code

---
+++ picSketch.v0.1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace picSketch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sketch_Click(object sender, EventArgs e)
+        {
+            game start = new game();
+
+            start.startGame();
+
+            txt_char.Text = start.character;
+            txt_att.Text = start.attribute;
+        
+        }
+    }
+}
---
+++ picSketch.v0.1/game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace picSketch
+{
+    class game
+    {
+        public string character { get; set; }
+        public string attribute { get; set; }
+
+        public void startGame(){
+
+            string[] charArray = System.IO.File.ReadAllLines("chars.txt");
+            string[] attArray = System.IO.File.ReadAllLines("att.txt");
+
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            character = charArray[random.Next(0, charArray.Length)];
+            attribute = attArray[random.Next(0, attArray.Length)];
+        }
+
+    }
+}
